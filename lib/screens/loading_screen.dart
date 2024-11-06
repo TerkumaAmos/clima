a743b01faa275db3cca4b0bd984fe96aa908b99e
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-06 17:40:28 +0100

    new

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -17,10 +17,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
     try {
-      Position position = await Geolocator.getCurrentPosition();
-      print(position);
+      somethingThatExpectsLessThan10(12);
+      // Position position = await Geolocator.getCurrentPosition();
+      // print(position);
     } catch (e) {
       print(e);
+    }
+  }
+
+  void somethingThatExpectsLessThan10(int n) {
+    if (n > 10) {
+      throw " n is greater than 10, n should always be greater than 10.";
     }
   }
 
